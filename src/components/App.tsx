c68cc6de45b3de6a24fb4da037b716ad493e3ca3
Mike Smart <mike.smart@jadu.co.uk>
2018-01-01 21:54:50 +0000

    reload

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 
 export default class App extends React.Component {
     public render () {
-        return <pre>Hello, World</pre>;
+        return <pre>Hello, World!</pre>;
     }
 }
